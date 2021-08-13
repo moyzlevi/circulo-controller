867c447667f65154933a18258b459240ac125707
Moyses Pietsch <moyzlevi@gmail.com>
2021-08-13 14:35:33 -0300

    Project Spring Created

---
+++ src/main/java/com/moyses/circulo/circulocontroller/CirculoControllerApplication.java
@@ -0,0 +1,13 @@
+package com.moyses.circulo.circulocontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirculoControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirculoControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moyses/circulo/circulocontroller/CirculoControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moyses.circulo.circulocontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CirculoControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
